9d922c5a197c66204ffaa1fe9eda5009f7b1f3e3
Kathrine Stangeland <kathrine.kodehode@gmail.com>
2023-11-30 22:56:49 +0100

    fix

--- src/pages/addPart/check/CheckForm.tsx
+++ src/pages/addPart/check/CheckForm.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router'
 import { Button } from '../../../components/SubmitButton'
 import ProgressBar from '../../../components/progressBar/ProgressBar'
 import { FormContainer, FormRadio } from '../styles'
+import { StyledTextArea } from './styles'
 const CheckForm = () => {
     const [checked, setChecked] = useState<boolean>(false)
     const [error, setError] = useState<string>()
@@ -22,7 +23,7 @@ const CheckForm = () => {
             <h3>Pre-checks</h3>
             <span style={{ color: 'red' }}>{error}</span>
             <FormRadio>
-                <label>
+                <label htmlFor="checks">
                     <input
                         type="radio"
                         name="checks"
@@ -31,19 +32,16 @@ const CheckForm = () => {
                     I have performed all necessary checks before adding this
                     item to the system
                 </label>
-                <label>
-                    Describe what has been checked, and inform about deviations
-                    <br />
-                    <textarea
-                        rows={5}
-                        cols={40}
-                        style={{ backgroundColor: '#eeeeee' }}
-                    ></textarea>
+                <label htmlFor="textArea">
+                    Describe what has been checked, and inform about deviations{' '}
                 </label>
+                <StyledTextArea
+                    id="textArea"
+                    rows={5}
+                    cols={40}
+                ></StyledTextArea>{' '}
             </FormRadio>
-            <div style={{ display: 'flex', justifyContent: 'end' }}>
-                <Button onClick={handleClick}>Next</Button>
-            </div>
+            <Button onClick={handleClick}>Next</Button>
         </FormContainer>
     )
 }
---
+++ src/pages/addPart/check/styles.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+import { COLORS } from '../../../style/GlobalStyles'
+
+export const ButtonWrap = styled.div`
+    display: flex;
+    justify-content: end;
+`
+export const StyledTextArea = styled.textarea`
+    background-color: ${COLORS.white};
+    margin: 0 16px;
+    max-width: 500px;
+`
+export const StyledInput = styled.input`
+    margin: 0;
+    padding: 1rem;
+`
+
+export const FormRadio = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding-bottom: 16px;
+
+    label,
+    input {
+        padding: 8px;
+        margin-right: 1rem;
+    }
+    input {
+        transform: scale(1.5);
+        padding: 10rem;
+    }
+`
