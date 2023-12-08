5aa4f3b87292c6e5440582f5aaddaeacfb1e15b1
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-12-08 14:38:58 +0100

    refactor: using react query

--- src/pages/partDetails/PartInfo/PartInfo.tsx
+++ src/pages/partDetails/PartInfo/PartInfo.tsx
@@ -1,187 +1,143 @@
-import { useState } from 'react'
-import { useForm } from 'react-hook-form'
-// import { Item, UpdateItem } from '../../../services/apiTypes'
-// import { useUpdateItem } from '../../services/hooks/useUpdateItem'
-
+import { ChangeEvent, useState } from 'react'
 import { Item, UpdateItem } from '../../../services/apiTypes'
-import { Container, Edit, InfoContainer } from './styles'
+import { Container } from './styles'
+import { useUpdateItem } from '../../../services/hooks/useUpdateItem'
+import EditableField from './EditableField'
 
-const types = ['Unit', 'Assembly', 'Sub-Assembly', 'Part']
+export type ItemFields = 'Type' | 'Category' | 'Location' | 'P/N' | 'S/N' | 'Vendor'
 
-const PartInfo = ({ item }: { item: Item }) => {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<UpdateItem>()
+const PartInfo = ({ item, isLoading }: { item: Item; isLoading: boolean }) => {
+    const { mutate } = useUpdateItem(item.id)
 
-    // const { mutate } = useUpdateItem(item.id)
-    const [updatedFormData, setUpdatedFormData] = useState(item)
+    const [activeEditMode, setActiveEditMode] = useState<ItemFields | null>(null)
+    const [selectedType, setSelectedType] = useState(item.type || '')
+    const [updatedItem, setUpdatedItem] = useState(item)
+    const handleBlurType = () => {
+        mutate({
+            ...item,
+            type: selectedType,
+        })
+    }
 
+    const handleBlurCategory = () => {
+        mutate({
+            ...item,
+            category: updatedItem.category,
+        })
+    }
+    const handleBlurLocation = () => {
+        mutate({
+            ...item,
+            location: updatedItem.location,
+        })
+    }
+
+    const handleBlurSerialNumber = () => {
+        mutate({
+            ...item,
+            serialNumber: updatedItem.serialNumber,
+        })
+    }
+
+    const handleBlurProductNumber = () => {
+        mutate({
+            ...item,
+            productNumber: updatedItem.productNumber,
+        })
+    }
+
+    const handleBlurVendor = () => {
+        mutate({
+            ...item,
+            vendor: updatedItem.vendor,
+        })
+    }
+    const handleSelectChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setSelectedType(e.target.value)
+    }
+    const handleInputChange = (fieldName: keyof UpdateItem, value: string | undefined) => {
+        setUpdatedItem((prev) => ({
+            ...prev,
+            [fieldName]: value,
+        }))
+    }
+
+    if (isLoading) {
+        return <p>Loading.. </p>
+    }
     return (
         <form>
             <Container>
-                <div>
-                    <label>
-                        <strong>Type</strong>
-                    </label>
-                    <InfoContainer>
-                        <select
-                            style={{ width: '170px' }}
-                            {...register('type')}
+                <EditableField
+                    label="Type"
+                    defaultValue={selectedType ? selectedType : item.type}
+                    onBlur={handleBlurType}
+                    isSelect
+                    activeEditMode={activeEditMode}
+                    setActiveEditMode={setActiveEditMode}
+                    options={['Unit', 'Assembly', 'Sub-Assembly', 'Part']}
+                    handleSelectChange={handleSelectChange}
+                    selectedType={selectedType}
+                />
 
-                            /* value={updatedFormData.type || item.type || ''} */
-                        >
-                            {types.map((type, idx) => (
-                                <option key={idx}>{type}</option>
-                            ))}
-                        </select>
-                        {/* <Select
-                            registerString="type"
-                            register={register}
-                            defaultValue={item.type}
-                            options={types}
-                        /> */}
-                        {/* <p>
-                        {updatedFormData.serialNumber
-                            ? updatedFormData.serialNumber
-                            : item.serialNumber}
-                    </p> */}
-                        <Edit />
-                    </InfoContainer>
-                </div>
+                {
+                    <EditableField
+                        label="Category"
+                        defaultValue={item.category}
+                        onBlur={handleBlurCategory}
+                        activeEditMode={activeEditMode}
+                        setActiveEditMode={setActiveEditMode}
+                        handleInputChange={(value) => handleInputChange('category', value)}
+                    />
+                }
 
-                <div>
-                    <label>
-                        <strong>CATEGORY</strong>
-                    </label>
-                    <InfoContainer>
-                        <input
-                        /* defaultValue={
-                                updatedFormData.vendor ? updatedFormData.vendor : item.vendor
-                            } */
-                        /* {...register('category')} */
-                        />
-                        {/* <p>
-                        {updatedFormData.serialNumber
-                            ? updatedFormData.serialNumber
-                            : item.serialNumber}
-                    </p> */}
-                        <Edit />
-                    </InfoContainer>
-                </div>
-                <div>
-                    <label>
-                        <strong>LOCATION</strong>
-                    </label>
-                    <InfoContainer>
-                        <input
-                            defaultValue={
-                                updatedFormData?.location
-                                    ? updatedFormData?.location
-                                    : item?.location || 'location'
-                            }
-                            {...register('location')}
-                        />
+                <EditableField
+                    label="Location"
+                    defaultValue={item.location}
+                    activeEditMode={activeEditMode}
+                    setActiveEditMode={setActiveEditMode}
+                    onBlur={handleBlurLocation}
+                    handleInputChange={(value) => handleInputChange('location', value)}
+                />
 
-                        {/* <p>
-                        {updatedFormData.serialNumber
-                            ? updatedFormData.serialNumber
-                            : item.serialNumber}
-                    </p> */}
-                        <Edit />
-                    </InfoContainer>
-                </div>
                 <div>
                     <label>
                         <strong>ADDED BY</strong>
                     </label>
                     <p>
-                        {item.addedByFirstName === null &&
-                        item.addedByLastName === null
+                        {item.addedByFirstName === null && item.addedByLastName === null
                             ? 'Not specified'
                             : `${item.addedByFirstName} ${item.addedByLastName}`}
                     </p>
                 </div>
-                <div>
-                    <label>
-                        <strong>S/N</strong>
-                    </label>
-                    <InfoContainer>
-                        <input
-                            /* defaultValue={
-                                updatedFormData.serialNumber
-                                    ? updatedFormData.serialNumber
-                                    : item.serialNumber
-                            } */
+                <EditableField
+                    label="S/N"
+                    defaultValue={item.serialNumber}
+                    activeEditMode={activeEditMode}
+                    setActiveEditMode={setActiveEditMode}
+                    onBlur={handleBlurSerialNumber}
+                    handleInputChange={(value) => handleInputChange('serialNumber', value)}
+                />
 
-                            {...register('serialNumber')}
-                        />
-
-                        {/* <p>
-                            {updatedFormData.serialNumber
-                                ? updatedFormData.serialNumber
-                                : item.serialNumber}
-                        </p> */}
-                        <Edit />
-                    </InfoContainer>
-                </div>
-
-                <div>
-                    <label
-                        style={{
-                            lineHeight: '3rem',
-                        }}
-                    >
-                        <strong>P/N</strong>
-                    </label>
-                    <InfoContainer>
-                        {item.productNumber && (
-                            <input
-                                defaultValue={
-                                    updatedFormData.productNumber
-                                        ? updatedFormData.productNumber
-                                        : item.productNumber
-                                }
-                                {...register('productNumber')}
-                            />
-                        )}
-                        {/* <p>
-                            {updatedFormData.productNumber
-                                ? updatedFormData.productNumber
-                                : item.productNumber}
-                        </p> */}
-                        <Edit />
-                    </InfoContainer>
-                </div>
-
-                <div>
-                    <label>
-                        <strong>VENDOR</strong>
-                    </label>
-                    <InfoContainer>
-                        <input
-                            defaultValue={
-                                updatedFormData.vendor
-                                    ? updatedFormData.vendor
-                                    : item.vendor
-                            }
-                            {...register('vendor')}
-                        />
-                        {/* <p>{updatedFormData.vendor ? updatedFormData.vendor : item.vendor}</p> */}
-                        <Edit />
-                    </InfoContainer>
-                </div>
+                <EditableField
+                    label="P/N"
+                    defaultValue={item.productNumber}
+                    onBlur={handleBlurProductNumber}
+                    activeEditMode={activeEditMode}
+                    setActiveEditMode={setActiveEditMode}
+                    handleInputChange={(value) => handleInputChange('productNumber', value)}
+                />
+                <EditableField
+                    label="Vendor"
+                    defaultValue={item.vendor}
+                    onBlur={handleBlurVendor}
+                    activeEditMode={activeEditMode}
+                    setActiveEditMode={setActiveEditMode}
+                    handleInputChange={(value) => handleInputChange('vendor', value)}
+                />
             </Container>
         </form>
     )
 }
 
 export default PartInfo
-
-/* <div>
-        <p>
-            <strong>Type</strong>
-        </p>
-        <input name="type" value={type} onChange={handleChange} />
-    </div> */
