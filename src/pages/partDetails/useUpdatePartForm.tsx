fd74f359d0ea637212bc0f1b1b421b031d018926
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-12-13 12:58:21 +0100

    refactor: fixing validation

--- src/pages/partDetails/useUpdatePartForm.tsx
+++ src/pages/partDetails/useUpdatePartForm.tsx
@@ -1,7 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { useLocation } from 'react-router'
-import { useUpdateItem } from '../../services/hooks/Items/useUpdateItem'
 
 import { z } from 'zod'
 
@@ -9,20 +8,22 @@ const partSchemaTest = z.object({
     id: z.string(),
     wpId: z.string(),
     serialNumber: z.string().min(1, 'Serial number is required'),
-    productNumber: z.string().min(1, 'Product number is required'),
+    productNumber: z
+        .string()
+        .min(1, 'Product number is required')
+        .max(20, 'Product number must be at most 20 characters'),
     type: z.string().min(1),
     location: z.string().nullish(),
     description: z.string(),
-    vendor: z.string().min(1, 'Vendor is required'),
-    addedById: z.string(),
-    comment: z.string(),
+    /* vendor: z.string().min(1, 'Vendor is required'), */
+    // addedById: z.string(),
+    // comment: z.string(),
     parentId: z.string().nullish(),
     category: z.string().nullish(),
 })
 export type PartSchemaTest = z.infer<typeof partSchemaTest>
 
-export const useUpdatePartForm = (id: string, item: any) => {
-    const { mutate } = useUpdateItem(id)
+export const useUpdatePartForm = (item: any) => {
     const appLocation = useLocation()
 
     const methods = useForm<PartSchemaTest>({
