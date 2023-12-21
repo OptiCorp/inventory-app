0b3aa90ca22305bcf2f21c6711e2c08521dcfd42
Henrik <henrik.laland@bouvet.no>
2023-12-21 13:00:03 +0100

    refactor of searcj admin

--- src/pages/admin/location/Locations.tsx
+++ src/pages/admin/location/Locations.tsx
@@ -1,19 +1,30 @@
 import { useDebounce } from 'usehooks-ts'
 import { useGetLocationsInfinite } from '../../../services/hooks/Locations/useGetLocationsInfinite'
 import { AdminContainer, ButtonContainer, SearchResultContainer } from '../styles'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import SearchBar from '../../../components/searchBar/SearchBar'
 import AdminSearchCard, { SearchType } from '../../../components/admin/AdminSearchCard'
 import { Button } from '../../../components/Button/SubmitButton'
 import { useNavigate } from 'react-router-dom'
 import { useGetLocations } from '../../../services/hooks/Locations/useGetLocations'
+import { Location } from '../../../services/apiTypes'
 
 const Locations = () => {
     const [searchTerm, setSearchTerm] = useState<string>('')
     const debouncedSearchTerm = useDebounce(searchTerm, 500)
-    const { data, isLoading, fetchNextPage } = useGetLocationsInfinite(debouncedSearchTerm)
     const { data: initialData } = useGetLocations()
+    const [filteredData, setFilteredData] = useState<Location[]>([])
     const navigate = useNavigate()
+
+    useEffect(() => {
+        if (initialData) {
+            setFilteredData(
+                initialData.filter((location) =>
+                    location.name.toLowerCase().includes(debouncedSearchTerm.toLowerCase())
+                )
+            )
+        }
+    }, [initialData, debouncedSearchTerm])
 
     return (
         <AdminContainer>
@@ -22,31 +33,13 @@ const Locations = () => {
                 setSearchTerm={setSearchTerm}
                 placeholder="Search for location"
             />
-            {data ? (
-                <SearchResultContainer>
-                    {data?.pages.map((page, i) =>
-                        page.map((location, index) => (
-                            <div
-                                id={
-                                    i === data.pages.length - 1 && index === page.length - 1
-                                        ? 'lastItem'
-                                        : ''
-                                }
-                            >
-                                <AdminSearchCard searchType={SearchType.Location} data={location} />
-                            </div>
-                        ))
-                    )}
-                </SearchResultContainer>
-            ) : (
-                <SearchResultContainer>
-                    {initialData?.map((location, i) => (
-                        <div id={i === initialData.length - 1 ? 'lastItem' : ''}>
-                            <AdminSearchCard searchType={SearchType.Location} data={location} />
-                        </div>
-                    ))}
-                </SearchResultContainer>
-            )}
+            <SearchResultContainer>
+                {filteredData?.map((location, i) => (
+                    <div id={i === filteredData.length - 1 ? 'lastItem' : ''}>
+                        <AdminSearchCard searchType={SearchType.Location} data={location} />
+                    </div>
+                ))}
+            </SearchResultContainer>
             <ButtonContainer>
                 <Button
                     onClick={() => navigate('/admin/add-location')}
