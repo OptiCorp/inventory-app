208fad4aed4a3aa2463c163d74d8a266b382ffb1
Eirik Bakkan <eirikbakkan@gmail.com>
2023-12-22 14:24:48 +0100

    added max width

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { MsalProvider } from '@azure/msal-react'
 import { pca } from './msalConfig.ts'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { AppContainer, BodyWrapper } from './styles.ts'
 
 const queryClient = new QueryClient()
 
@@ -12,7 +13,11 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
             <MsalProvider instance={pca}>
-                <App />
+                <BodyWrapper>
+                    <AppContainer>
+                        <App />
+                    </AppContainer>
+                </BodyWrapper>
             </MsalProvider>
             <ReactQueryDevtools />
         </QueryClientProvider>
