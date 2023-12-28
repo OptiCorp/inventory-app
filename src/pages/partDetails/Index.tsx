f8e8d9b3534fb440fca2d4c33dec72019ee40a9d
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-12-28 12:32:55 +0100

    feat: breadcrumb

--- src/pages/partDetails/Index.tsx
+++ src/pages/partDetails/Index.tsx
@@ -1,5 +1,5 @@
-import { StyledContainerDiv } from './styles'
-import { useParams } from 'react-router-dom'
+import { BreadcrumbLink, BreadcrumbsMargin, StyledContainerDiv } from './styles'
+import { useNavigate, useParams } from 'react-router-dom'
 import { useGetItemById } from '../../services/hooks/Items/useGetItemById'
 import { Comments } from './Comments/Comments'
 import { FormProvider } from 'react-hook-form'
@@ -8,15 +8,32 @@ import { Log } from './Log'
 import PartInfo from './PartInfo/PartInfo'
 import { useUpdatePartForm } from './useUpdatePartForm'
 import { ExampleUpload } from '../../components/Upload/Upload'
-import { it } from 'node:test'
+import { Breadcrumbs } from '@mui/material'
 
 const PartDetails = () => {
     const { id } = useParams() as { id: string }
     const { data: item, isLoading } = useGetItemById(id)
     const { methods } = useUpdatePartForm(item!)
+    const navigate = useNavigate()
     if (!item) return null
+
     return (
         <StyledContainerDiv>
+            {item.parentId && (
+                <BreadcrumbsMargin>
+                    <Breadcrumbs>
+                        <BreadcrumbLink
+                            onClick={() => navigate(`/${item.parentId}`)}
+                            underline="none"
+                        >
+                            {item.parent?.id}
+                        </BreadcrumbLink>
+                        <BreadcrumbLink onClick={() => navigate(`/${item.id}`)} underline="none">
+                            {item.id}
+                        </BreadcrumbLink>
+                    </Breadcrumbs>
+                </BreadcrumbsMargin>
+            )}
             <FormProvider {...methods}>
                 <PartInfo item={item} isLoading={isLoading} />
                 <Hierarchy item={item} />
