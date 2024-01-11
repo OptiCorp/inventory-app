479c3cd1e4b848c26901678aa667886f70338ffb
bjornole-kodehode <bjornole.kodehode@gmail.com>
2024-01-11 13:34:26 +0100

    feat: addChildItemToParent & removeParentFromItem

--- src/services/api.ts
+++ src/services/api.ts
@@ -77,7 +77,7 @@ const apiService = () => {
     }
 
     // Generic function for post requests
-    const postByFetch = async <T>(url: string, bodyData: T) => {
+    const postByFetch = async <T>(url: string, bodyData?: T) => {
         try {
             const tokenResponse = await pca.acquireTokenSilent(request)
             const postOperation = {
@@ -118,7 +118,7 @@ const apiService = () => {
 
     // Generic function for put requests
     const putByFetch = async <T>(url: string, bodyData: T) => {
-        try {
+
             const tokenResponse = await pca.acquireTokenSilent(request)
             const putOperations = {
                 method: 'PUT',
@@ -132,10 +132,7 @@ const apiService = () => {
             const res = await fetch(`${API_URL}/${url}`, putOperations)
 
             return res
-        } catch (error) {
-            console.error('An error occurred:', error)
-            throw error
-        }
+       
     }
 
     // Generic function for delete requests
@@ -295,6 +292,10 @@ const apiService = () => {
         return await putByFetch(`Item/${id}?updatedById=${updatedById}`, item)
     }
 
+    const removeParentIdFromItem = async (itemId: string): Promise<Response> => {
+        return await postByFetch(`Item/RemoveParentId?itemId=${itemId}`)
+    } 
+
     const isWpIdUnique = async (id: string) => {
         if (!id) return
         return await getByFetch(`Item/IsWpIdUnique/${id}`)
@@ -329,6 +330,10 @@ const apiService = () => {
         })
         return documentResponses
     }
+
+    const addChildItemToParent = async (itemId: string, childItemId: string): Promise<Response> => {
+        return await postByFetch(`Item/AddChildItemToParent?itemId=${itemId}&childItemId=${childItemId}`)
+    } 
 
     const addItemsToList = async (listId: string, itemId: string): Promise<Response> => {
         return await postByFetch(`List/AddItems/?listId=${listId}`, [itemId])
@@ -455,6 +460,7 @@ const apiService = () => {
         getItemsNotInListBySearchString,
         getItemsByUserId,
         addItem,
+        addChildItemToParent,
         getListsBySearchString,
         getListsByUserId,
         addList,
@@ -462,6 +468,7 @@ const apiService = () => {
         updateList,
         getItemById,
         updateItemById,
+        removeParentIdFromItem,
         getListById,
         addItemsToList,
         removeItemsFromList,
