695696c7959db137588910a9d858caeb5fbd273e
bjornole-kodehode <bjornole.kodehode@gmail.com>
2024-01-11 13:46:29 +0100

    refactor: using Select component

--- src/pages/partDetails/PartInfo/TypeField.tsx
+++ src/pages/partDetails/PartInfo/TypeField.tsx
@@ -1,8 +1,9 @@
-import { Box, ClickAwayListener, MenuItem } from '@mui/material'
+import { Box, ClickAwayListener } from '@mui/material'
 import { useFormContext } from 'react-hook-form'
-import { Edit, LabelContainer, StyledTextField, TextBoxWrap, TypeContainer } from './styles'
+import { Edit, LabelContainer, TextBoxWrap } from './styles'
 import { TypeProps } from './types'
 import { useState } from 'react'
+import Select, { CSSObjectWithLabel } from 'react-select'
 
 export const TypeField = ({
     handleSelectChange,
@@ -21,6 +22,40 @@ export const TypeField = ({
         setIsOpen(true)
     }
 
+    const newOptions = options.map((option) => ({
+        value: option.id,
+        label: option.name,
+    }))
+
+    const customStyles = {
+        control: (provided: CSSObjectWithLabel) => ({
+            ...provided,
+            width: '100%',
+            maxWidth: '500px',
+            height: '41px',
+            backgroundColor: '#fff',
+            borderRadius: 0,
+            border: '0px',
+            borderBottom: '1px solid #000',
+        }),
+        menu: (provided: CSSObjectWithLabel) => ({
+            ...provided,
+            width: '70%',
+            maxWidth: '500px',
+        }),
+        option: (
+            provided: CSSObjectWithLabel,
+            state: { data: { value: string } }
+        ): CSSObjectWithLabel => {
+            const stateValue = state.data.value
+            return {
+                ...provided,
+                color: stateValue === defaultValue ? 'gray' : 'inherit',
+                cursor: stateValue === defaultValue ? 'not-allowed' : 'pointer',
+            }
+        },
+    }
+
     return (
         <TextBoxWrap>
             <ClickAwayListener onClickAway={handleClickAway}>
@@ -29,30 +64,24 @@ export const TypeField = ({
                         <label>
                             <strong>{label.toUpperCase()}</strong>
                         </label>
-                        <Edit onClick={handleEditClick} />
-                    </LabelContainer>
-                    <TypeContainer>
-                        <StyledTextField
-                            {...register(label.toLowerCase())}
-                            onBlur={onBlur}
-                            select
-                            onChange={handleSelectChange}
-                            $isOpen={isOpen}
-                            fullWidth
-                            variant="standard"
-                            value={defaultValue}
-                            InputProps={{
-                                disableUnderline: !isOpen,
-                                readOnly: !isOpen,
+                        <Edit
+                            onClick={() => {
+                                handleEditClick()
                             }}
-                        >
-                            {options.map((option) => (
-                                <MenuItem key={option} value={option}>
-                                    {option}
-                                </MenuItem>
-                            ))}
-                        </StyledTextField>
-                    </TypeContainer>
+                        />
+                    </LabelContainer>
+                    {isOpen && (
+                        <Select
+                            {...register(label.toLowerCase())}
+                            options={newOptions}
+                            onChange={handleSelectChange}
+                            onBlur={onBlur}
+                            styles={customStyles}
+                        />
+                    )}
+                    {!isOpen && (
+                        <p>{newOptions.find((option) => option.value === defaultValue)?.label}</p>
+                    )}
                 </Box>
             </ClickAwayListener>
         </TextBoxWrap>
