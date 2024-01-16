e0af54c9da6b8079b32e925ec367cf10384529aa
Benjamin Lyon <benjaminlyon0@gmail.com>
2024-01-16 14:37:38 +0100

    refactor: move functions and styles outside of jsx

--- src/components/ResultSearchCard/ResultSearchCard.tsx
+++ src/components/ResultSearchCard/ResultSearchCard.tsx
@@ -11,21 +11,23 @@ type Props = {
 const SearchResultCard = ({ part, icon }: Props) => {
     const navigate = useNavigate();
 
+    const handleClick = () => {
+        if (location.pathname === '/search' || location.pathname === '/add-part') {
+            navigate(`/${part.id}`);
+        }
+    };
+
+    const cursorStyle =
+        location.pathname === '/search' || location.pathname === '/add-part'
+            ? 'pointer'
+            : undefined;
+
     return (
-        <>
-            <ResultCardContainer
-                style={{
-                    cursor: `${location.pathname === `/search` ?? '/add-part' ? `pointer` : null}`,
-                }}
-                onClick={() => {
-                    location.pathname === '/search' ?? '/add-part' ? navigate(`/${part.id}`) : null;
-                }}
-            >
-                <SearchCard title="">
-                    <Searchinfo part={part} icon={icon} />
-                </SearchCard>
-            </ResultCardContainer>
-        </>
+        <ResultCardContainer style={{ cursor: cursorStyle }} onClick={handleClick}>
+            <SearchCard title="">
+                <Searchinfo part={part} icon={icon} />
+            </SearchCard>
+        </ResultCardContainer>
     );
 };
 
