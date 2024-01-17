e0be69d92ee0f3205d00f88369b64052e971831e
Benjamin Lyon <benjaminlyon0@gmail.com>
2024-01-17 12:40:04 +0100

    feat: add the first button story

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button } from './Button';
+
+const meta = {
+    title: 'Components/Button',
+    component: Button,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {
+        backgroundColor: 'red',
+        children: 'Button',
+        height: '50px',
+    }
+}
