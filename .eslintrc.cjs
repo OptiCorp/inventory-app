37859a92d7e836b3a3610446c1ece30962e8c1ee
Benjamin Lyon <benjaminlyon0@gmail.com>
2024-01-17 14:11:24 +0100

    feat: add storybook example

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,16 @@
 module.exports = {
     root: true,
     env: { browser: true, es2020: true },
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-type-checked', 'plugin:@typescript-eslint/stylistic-type-checked', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'plugin:import/typescript', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:import/typescript',
+        'plugin:prettier/recommended',
+        'plugin:storybook/recommended',
+    ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
