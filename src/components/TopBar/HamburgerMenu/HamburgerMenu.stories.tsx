321bf612f4a8e64b0aeca3e64dbc22072f387882
Benjamin Lyon <benjaminlyon0@gmail.com>
2024-01-18 15:41:58 +0100

    feat: add new HamburgetMenu story

---
+++ src/components/TopBar/HamburgerMenu/HamburgerMenu.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { HamburgerMenu } from './HamburgerMenu';
+
+const meta = {
+    title: 'Components/HamburgerMenu',
+    component: HamburgerMenu,
+    tags: ['autodocs'],
+} as Meta<typeof HamburgerMenu>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {
+        setHamburgerIsOpen: () => {
+            console.log('Hamburger menu clicked');
+        },
+    },
+};
