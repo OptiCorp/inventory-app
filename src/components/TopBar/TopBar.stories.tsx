3bead514fc95e6f87d8a38e51ac2a4a0bebb19e0
Benjamin Lyon <benjaminlyon0@gmail.com>
2024-01-18 15:46:23 +0100

    feat: add new TopBar story

---
+++ src/components/TopBar/TopBar.stories.tsx
@@ -0,0 +1,15 @@
+import { Meta, StoryObj } from '@storybook/react';
+import TopBar from './TopBar';
+
+const meta = {
+    title: 'Components/TopBar',
+    component: TopBar,
+    tags: ['autodocs'],
+} as Meta<typeof TopBar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {},
+};
