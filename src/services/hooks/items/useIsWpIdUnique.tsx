f67f3c9e0a8cacbab9c5f335af9535db49b3a747
bjornole-kodehode <bjornole.kodehode@gmail.com>
2024-01-29 07:39:53 +0100

    feat: enable fetch argument, true as default

--- src/services/hooks/items/useIsWpIdUnique.tsx
+++ src/services/hooks/items/useIsWpIdUnique.tsx
@@ -1,9 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import apiService from '../../api';
 
-export const useIsWpIdUnique = (id: string) => {
+export const useIsWpIdUnique = (id: string, enabled = true) => {
     return useQuery({
         queryKey: ['wpId', id],
         queryFn: () => apiService().isWpIdUnique(id),
+        enabled: !!enabled,
     });
 };
