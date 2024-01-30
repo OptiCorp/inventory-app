408a6ec4515842e11d792cafe56d7be0d90bf619
bjornole-kodehode <bjornole.kodehode@gmail.com>
2024-01-30 13:53:09 +0100

    feat: delete item functionality + fix breadcrumb

--- src/pages/partDetails/Index.tsx
+++ src/pages/partDetails/Index.tsx
@@ -11,6 +11,11 @@ import { Hierarchy } from './hierarchy';
 import PartInfo from './partInfo/PartInfo';
 import { useUpdatePartForm } from './partInfo/hooks/useUpdatePartForm';
 import { BreadcrumbLink, BreadcrumbsMargin, StyledContainerDiv } from './styles';
+import { Button } from '../../components/Button/Button';
+import { ButtonContainer } from '../addPart/styles';
+import { useDeleteItemById } from '../../services/hooks/items/useDeleteItemById';
+import { useState } from 'react';
+import CustomDialog from '../../components/CustomDialog/CustomDialog';
 
 const PartDetails = () => {
     const { id } = useParams() as { id: string };
@@ -18,8 +23,21 @@ const PartDetails = () => {
     const { methods } = useUpdatePartForm(item!);
     const navigate = useNavigate();
     const { width } = useWindowDimensions();
+    const { data: parentItem } = useGetItemById(item?.parentId ?? '');
+    const { mutate: deleteItem } = useDeleteItemById(item?.id ?? '');
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
     if (!item) return null;
-    console.log(item);
+
+    const handleClose = () => {
+        setIsOpen(false);
+    };
+    const handleDelete = () => {
+        setIsOpen(false);
+        deleteItem();
+        navigate('/');
+    };
+
     return (
         <StyledContainerDiv>
             {item.parentId && (
@@ -29,10 +47,10 @@ const PartDetails = () => {
                             onClick={() => navigate(`/${item.parentId}`)}
                             underline="none"
                         >
-                            {item.parent?.id}
+                            {parentItem?.itemTemplate?.type}: {item.parent?.wpId}
                         </BreadcrumbLink>
                         <BreadcrumbLink onClick={() => navigate(`/${item.id}`)} underline="none">
-                            {item.id}
+                            {item.itemTemplate.type}: {item.wpId}
                         </BreadcrumbLink>
                     </Breadcrumbs>
                 </BreadcrumbsMargin>
@@ -45,6 +63,19 @@ const PartDetails = () => {
                 <Comments item={item} />
                 <Log item={item} />
             </FormProvider>
+            <ButtonContainer>
+                <Button variant="red" type="button" onClick={() => setIsOpen(true)}>
+                    Delete
+                </Button>
+            </ButtonContainer>
+            <CustomDialog
+                open={isOpen}
+                SubmitButtonOnClick={handleDelete}
+                CancelButtonOnClick={handleClose}
+                title="Delete item"
+            >
+                Are you sure you want to permanently delete &apos;{item.wpId}&apos;?
+            </CustomDialog>
         </StyledContainerDiv>
     );
 };
