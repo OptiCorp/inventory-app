05bf3a92143fd0da103da2003e332b1134b1dbf8
bjornole-kodehode <bjornole.kodehode@gmail.com>
2024-01-30 13:09:56 +0100

    feat: comment functionality

--- src/pages/partDetails/comments/Comments.tsx
+++ src/pages/partDetails/comments/Comments.tsx
@@ -1,42 +1,90 @@
 import { TextField } from '@mui/material';
-import { ChangeEvent, useState } from 'react';
+import { FormEvent, useContext } from 'react';
 import { Button } from '../../../components/Button/Button';
 import { Item } from '../../../services/apiTypes';
 import { Wrapper } from './styles';
+import { Controller, useFormContext } from 'react-hook-form';
+import { PartInfoSchema } from '../partInfo/hooks';
+import { useUpdateItem } from '../../../services/hooks/items/useUpdateItem';
+import UmAppContext from '../../../contexts/UmAppContext';
+import { handleApiRequestSnackbar } from '../../../utils/handleApiRequestSnackbar';
 
 export const Comments = ({ item }: { item: Item }) => {
-    const [content, setContent] = useState(item?.comment);
+    const {
+        watch,
+        register,
+        formState: { dirtyFields },
+        control,
+    } = useFormContext<PartInfoSchema>();
+    const { currentUser, setSnackbarSeverity, setSnackbarText } = useContext(UmAppContext);
+    const { mutate } = useUpdateItem(item?.id, currentUser!.id);
+
+    const commentValue = watch('comment');
+    const onSubmit = (event: FormEvent) => {
+        event.preventDefault();
+        if (dirtyFields.comment) {
+            mutate(
+                {
+                    ...item,
+                    comment: commentValue,
+                },
+                {
+                    onSuccess: (responseData) => {
+                        handleApiRequestSnackbar(
+                            responseData,
+                            'Comment updated.',
+                            setSnackbarSeverity,
+                            setSnackbarText
+                        );
+                    },
+                }
+            );
+        }
+    };
+
     return (
         <>
-            <div>
+            <h4>Comments</h4>
+            <form onSubmit={onSubmit}>
                 <div>
-                    {' '}
-                    <TextField
-                        id="filled-multiline-static"
-                        multiline
-                        rows={5}
-                        defaultValue={item.comment ?? content}
-                        fullWidth
-                        onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                            setContent(e.target.value);
-                        }}
-                        key={item?.wpId ?? ''}
-                        variant="filled"
-                        sx={{
-                            '&::before': {
-                                display: 'none',
-                            },
-                            '&:focus-within': {
-                                outline: '2px solid var(--Textarea-focusedHighlight)',
-                                outlineOffset: '2px',
-                            },
+                    <Controller
+                        control={control}
+                        name="comment"
+                        render={(controllerProps) => {
+                            const {
+                                field: { onChange },
+                            } = controllerProps;
+
+                            return (
+                                <TextField
+                                    id="filled-multiline-static"
+                                    {...register('comment')}
+                                    multiline
+                                    rows={5}
+                                    fullWidth
+                                    onChange={onChange}
+                                    key={item?.wpId ?? ''}
+                                    variant="filled"
+                                    sx={{
+                                        '&::before': {
+                                            display: 'none',
+                                        },
+                                        '&:focus-within': {
+                                            outline: '2px solid var(--Textarea-focusedHighlight)',
+                                            outlineOffset: '2px',
+                                        },
+                                    }}
+                                />
+                            );
                         }}
                     />
                 </div>
                 <Wrapper>
-                    <Button variant="white">Sumbit</Button>
+                    <Button type="submit" variant="white">
+                        Sumbit
+                    </Button>
                 </Wrapper>
-            </div>
+            </form>
         </>
     );
 };
