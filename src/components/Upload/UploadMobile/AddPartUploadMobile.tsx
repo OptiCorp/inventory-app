763386d7099ddaaf99c4d4f306e0d94aa5cac33a
Henrik <henrik.laland@bouvet.no>
2024-01-31 14:11:37 +0100

    Refactor addpartuploadmobile

--- src/components/Upload/UploadMobile/AddPartUploadMobile.tsx
+++ src/components/Upload/UploadMobile/AddPartUploadMobile.tsx
@@ -1,22 +1,30 @@
 import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
-import { useEffect, useRef, useState } from 'react';
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
 import { useFormContext } from 'react-hook-form';
-import { Button } from '../../Button/Button.tsx';
-import { Container, Wrapper } from './styles';
+import { Wrapper } from './styles';
 import File from '../../File/File.tsx';
+import { Box } from '@mui/material';
+import { Button as SubmitButton } from '../../Button/Button';
 
 const AddPartUploadMobile = () => {
-    const { register, setValue } = useFormContext();
+    const { setValue, getValues } = useFormContext();
     const [files, setFiles] = useState<File[]>();
-    const documentationField = register('files');
     const inputFile = useRef<HTMLInputElement | null>(null);
     const [showArrow, setShowArrow] = useState(true);
 
     const handleFileRemoval = (index: number) => {
         const filesCopy = [...files!];
         filesCopy.splice(index, 1);
+        setValue('files', filesCopy);
         setFiles(filesCopy);
     };
+
+    const handleFileUpload = (e: ChangeEvent<HTMLInputElement>) => {
+        const files: File[] = getValues('files') as File[];
+        setValue('files', files ? files.concat([...e.target.files!]) : [...e.target.files!]);
+        setFiles(getValues('files') as File[]);
+    };
+
     const handleIntersect = (entries: IntersectionObserverEntry[]) => {
         entries.forEach((entry) => {
             if (entry.isIntersecting) {
@@ -26,12 +34,13 @@ const AddPartUploadMobile = () => {
             }
         });
     };
+
     const observer = new IntersectionObserver(handleIntersect, {
         threshold: 0.5,
     });
+
     useEffect(() => {
-        setValue('files', files);
-        const fileClass = document.getElementsByClassName('fileClass');
+        const fileClass = document.getElementsByClassName('files');
         if (fileClass.length !== 0) {
             for (const element of fileClass) {
                 observer.observe(element);
@@ -45,6 +54,7 @@ const AddPartUploadMobile = () => {
             }
         };
     }, [files]);
+
     return (
         <>
             <Wrapper onTouchMove={() => setShowArrow(false)}>
@@ -62,26 +72,20 @@ const AddPartUploadMobile = () => {
                     />
                 )}
             </Wrapper>
-            <Container>
-                <Button variant="white" onClick={() => inputFile.current?.click()}>
+            <Box sx={{ display: 'flex', justifyContent: 'end' }}>
+                <SubmitButton variant="white" onClick={() => inputFile.current?.click()}>
                     {' '}
                     <input
                         type="file"
                         multiple
                         accept=".pdf,.png,.docx,.jpg"
                         style={{ display: 'none' }}
-                        {...documentationField}
-                        onChange={(e) => {
-                            setFiles([...e.target.files!]);
-                        }}
-                        ref={(e) => {
-                            documentationField.ref(e);
-                            inputFile.current = e;
-                        }}
+                        onChange={handleFileUpload}
+                        ref={inputFile}
                     />
                     UPLOAD NEW
-                </Button>
-            </Container>
+                </SubmitButton>
+            </Box>
         </>
     );
 };
