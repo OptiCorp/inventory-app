89e908ccd397969c38338c1485583bd59939d527
Kathrine-Kodehode <112854862+Kodehodekatty@users.noreply.github.com>
2024-02-01 15:09:15 +0100

    Update useUpdateItemTemplate.tsx

--- src/services/hooks/template/useUpdateItemTemplate.tsx
+++ src/services/hooks/template/useUpdateItemTemplate.tsx
@@ -5,28 +5,20 @@ export type UpdateItemTemplate = {
     id: string;
     itemTemplate: TemplateSchema;
 };
-/**
- *
- * @param id The item template id
- */
+
 export const useUpdateItemTemplate = (id: string) => {
     const api = apiService();
     const queryClient = useQueryClient();
     return useMutation({
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
         mutationFn: (itemTemplate: TemplateSchema) => api.updateItemTemplateById(id, itemTemplate),
         onSuccess: () => {
             queryClient
                 .invalidateQueries({
-                    queryKey: ['itemTemplates'],
+                    queryKey: ['itemTemplate'],
                 })
                 .catch((error) => {
                     console.error(console.error('Failed to invalidate queries: ', error));
                 });
         },
-        onError: (error) => {
-            console.log('error', error);
-        },
-        throwOnError: true,
     });
 };
