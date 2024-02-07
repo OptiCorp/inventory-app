665ff7f3e2fa18f6b172d731fa91fe8450d8cefa
Benjamin Lyon <benjaminlyon0@gmail.com>
2024-02-07 13:40:48 +0100

    feat: add new test for `removeItemsFromList`

--- tests/unit/services/api.test.ts
+++ tests/unit/services/api.test.ts
@@ -2,10 +2,13 @@ import { it, expect, describe, vi } from 'vitest';
 import '@testing-library/jest-dom';
 import apiService from '../../../src/services/api.ts';
 
+// TODO: Implement tests for the response of the API service using Mock Service Worker.
+
 vi.mock('../../../src/services/api.ts', () => {
     return {
         default: () => ({
             addItemsToList: vi.fn(),
+            removeItemsFromList: vi.fn(),
         }),
     };
 });
@@ -15,11 +18,20 @@ describe('API Service', () => {
         const listId = '1';
         const itemId = '2';
         const addSubItems = false;
-
         const api = apiService();
 
         await api.addItemsToList(listId, itemId, addSubItems);
 
         expect(api.addItemsToList).toHaveBeenCalledWith(listId, itemId, addSubItems);
+    });
+
+    it('removeItemsFromList sends correct paramters', async () => {
+        const listId = '1';
+        const itemId = '2';
+        const api = apiService();
+
+        await api.removeItemsFromList(listId, itemId);
+
+        expect(api.removeItemsFromList).toHaveBeenCalledWith(listId, itemId);
     });
 });
