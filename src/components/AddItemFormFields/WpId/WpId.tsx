1d6859f10b0d3353f9f68d9e9f3007f0a2368747
bjornole-kodehode <bjornole.kodehode@gmail.com>
2024-02-07 15:36:37 +0100

    refactor: display every wp id as plain text (wip)

--- src/components/AddItemFormFields/WpId/WpId.tsx
+++ src/components/AddItemFormFields/WpId/WpId.tsx
@@ -10,10 +10,14 @@ import { ToolTip } from '../../ToolTip/ToolTip.tsx';
 import { StyledDiv, StyledErrorP, StyledIconContainer, StyledInputWrap } from '../styles.ts';
 import { StyledParagraph } from './styles.ts';
 
-export const WpId = () => {
+type WpIdProps = {
+    index?: number;
+    isEdit?: boolean;
+    wpId: string;
+};
+export const WpId = ({ index, isEdit, wpId }: WpIdProps) => {
     const { register, setValue } = useFormContext();
 
-    const [wpId, setWpId] = useState(uuid().slice(0, 8));
     const debouncedWpId = useDebounce(wpId, 500);
     const { data: isUnique, isLoading } = useIsWpIdUnique(debouncedWpId);
 
@@ -21,47 +25,54 @@ export const WpId = () => {
         setValue('wpId', wpId);
         setValue('uniqueWpId', isUnique);
     }, [setValue, wpId, isUnique]);
-
+    console.log(index);
     return (
-        <StyledDiv>
-            <StyledInputWrap>
-                <StyledIconContainer>
-                    <label htmlFor="WellPartner Id">WellPartner ID </label>
-                    <ToolTip content="Specify a unique WellPartner ID">
-                        <FaRegQuestionCircleIcon />
-                    </ToolTip>
-                </StyledIconContainer>
-                <ErrorMessage
-                    name="wpId"
-                    render={({ message }) => <StyledErrorP>{message}</StyledErrorP>}
-                />
-            </StyledInputWrap>
+        <>
+            {!isEdit && <p>{wpId}</p>}
+            {isEdit && (
+                <StyledDiv>
+                    <StyledInputWrap>
+                        <StyledIconContainer>
+                            <label htmlFor="WellPartner Id">
+                                WellPartner ID {index && `(${index})`}
+                            </label>
+                            <ToolTip content="Specify a unique WellPartner ID">
+                                <FaRegQuestionCircleIcon />
+                            </ToolTip>
+                        </StyledIconContainer>
+                        <ErrorMessage
+                            name="wpId"
+                            render={({ message }) => <StyledErrorP>{message}</StyledErrorP>}
+                        />
+                    </StyledInputWrap>
 
-            <TextField
-                id="filled-disabled"
-                sx={{ width: '100%' }}
-                label=""
-                placeholder="E.g 5321-1"
-                variant="filled"
-                {...register('wpId')}
-                value={wpId}
-                onChange={(e) => setWpId(e.target.value)}
-                inputProps={{ name: 'isUnique', 'data-isunique': isUnique }}
-            />
+                    <TextField
+                        id="filled-disabled"
+                        sx={{ width: '100%' }}
+                        label=""
+                        placeholder="E.g 5321-1"
+                        variant="filled"
+                        {...register('wpId')}
+                        value={wpId}
+                        onChange={(e) => setWpId(e.target.value)}
+                        inputProps={{ name: 'isUnique', 'data-isunique': isUnique }}
+                    />
 
-            {isLoading && <p>Checking...</p>}
-            {wpId && (
-                <>
-                    {isUnique === true && (
-                        <StyledParagraph>WellPartner ID is unique!</StyledParagraph>
+                    {isLoading && <p>Checking...</p>}
+                    {wpId && (
+                        <>
+                            {isUnique === true && (
+                                <StyledParagraph>WellPartner ID is unique!</StyledParagraph>
+                            )}
+                            {isUnique === false && (
+                                <StyledParagraph $isUnique={isUnique}>
+                                    WellPartner ID is not unique. Please choose a different one.
+                                </StyledParagraph>
+                            )}
+                        </>
                     )}
-                    {isUnique === false && (
-                        <StyledParagraph $isUnique={isUnique}>
-                            WellPartner ID is not unique. Please choose a different one.
-                        </StyledParagraph>
-                    )}
-                </>
+                </StyledDiv>
             )}
-        </StyledDiv>
+        </>
     );
 };
