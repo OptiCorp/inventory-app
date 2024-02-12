fac8f1f2efe3ac4de10b1e520e4f61f76d4d14c5
bjornole-kodehode <bjornole.kodehode@gmail.com>
2024-02-12 11:31:36 +0100

    refactor: moved wp id to parent

--- src/components/AddItemFormFields/WpId/WpId.tsx
+++ src/components/AddItemFormFields/WpId/WpId.tsx
@@ -4,75 +4,77 @@ import { useEffect, useState } from 'react';
 import { useFormContext } from 'react-hook-form';
 import { FaRegQuestionCircle as FaRegQuestionCircleIcon } from 'react-icons/fa';
 import { useDebounce } from 'usehooks-ts';
-import { v4 as uuid } from 'uuid';
 import { useIsWpIdUnique } from '../../../services/hooks/items/useIsWpIdUnique.tsx';
 import { ToolTip } from '../../ToolTip/ToolTip.tsx';
 import { StyledDiv, StyledErrorP, StyledIconContainer, StyledInputWrap } from '../styles.ts';
 import { StyledParagraph } from './styles.ts';
+import { ItemSchema } from '../../../pages/addItem/hooks/itemValidator.ts';
 
 type WpIdProps = {
     index?: number;
-    isEdit?: boolean;
     wpId: string;
+    fieldName: string;
+    isPlainText?: boolean;
+    onChange: (value: string) => void;
 };
-export const WpId = ({ index, isEdit, wpId }: WpIdProps) => {
-    const { register, setValue } = useFormContext();
 
+export const WpId = ({ fieldName, wpId, onChange, isPlainText }: WpIdProps) => {
+    const { register, setValue } = useFormContext<ItemSchema>();
+    const [inputValue, setInputValue] = useState(wpId);
     const debouncedWpId = useDebounce(wpId, 500);
     const { data: isUnique, isLoading } = useIsWpIdUnique(debouncedWpId);
 
     useEffect(() => {
-        setValue('wpId', wpId);
-        setValue('uniqueWpId', isUnique);
-    }, [setValue, wpId, isUnique]);
-    console.log(index);
+        setValue(fieldName as keyof ItemSchema, inputValue);
+        setValue('uniqueWpId', isUnique!);
+        onChange(inputValue);
+    }, [setValue, inputValue, isUnique]);
+
+    if (isPlainText) {
+        return <p>{wpId}</p>;
+    }
     return (
         <>
-            {!isEdit && <p>{wpId}</p>}
-            {isEdit && (
-                <StyledDiv>
-                    <StyledInputWrap>
-                        <StyledIconContainer>
-                            <label htmlFor="WellPartner Id">
-                                WellPartner ID {index && `(${index})`}
-                            </label>
-                            <ToolTip content="Specify a unique WellPartner ID">
-                                <FaRegQuestionCircleIcon />
-                            </ToolTip>
-                        </StyledIconContainer>
-                        <ErrorMessage
-                            name="wpId"
-                            render={({ message }) => <StyledErrorP>{message}</StyledErrorP>}
-                        />
-                    </StyledInputWrap>
-
-                    <TextField
-                        id="filled-disabled"
-                        sx={{ width: '100%' }}
-                        label=""
-                        placeholder="E.g 5321-1"
-                        variant="filled"
-                        {...register('wpId')}
-                        value={wpId}
-                        onChange={(e) => setWpId(e.target.value)}
-                        inputProps={{ name: 'isUnique', 'data-isunique': isUnique }}
+            <StyledDiv>
+                <StyledInputWrap>
+                    <StyledIconContainer>
+                        <label htmlFor="WellPartner Id">WellPartner ID</label>
+                        <ToolTip content="Specify a unique WellPartner ID">
+                            <FaRegQuestionCircleIcon />
+                        </ToolTip>
+                    </StyledIconContainer>
+                    <ErrorMessage
+                        name="wpId"
+                        render={({ message }) => <StyledErrorP>{message}</StyledErrorP>}
                     />
+                </StyledInputWrap>
 
-                    {isLoading && <p>Checking...</p>}
-                    {wpId && (
-                        <>
-                            {isUnique === true && (
-                                <StyledParagraph>WellPartner ID is unique!</StyledParagraph>
-                            )}
-                            {isUnique === false && (
-                                <StyledParagraph $isUnique={isUnique}>
-                                    WellPartner ID is not unique. Please choose a different one.
-                                </StyledParagraph>
-                            )}
-                        </>
-                    )}
-                </StyledDiv>
-            )}
+                <TextField
+                    id="filled-disabled"
+                    sx={{ width: '100%' }}
+                    label=""
+                    placeholder="E.g 5321-1"
+                    variant="filled"
+                    {...register(fieldName as keyof ItemSchema)}
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    inputProps={{ name: 'isUnique', 'data-isunique': isUnique }}
+                />
+
+                {isLoading && <p>Checking...</p>}
+                {wpId && (
+                    <>
+                        {isUnique === true && (
+                            <StyledParagraph>WellPartner ID is unique!</StyledParagraph>
+                        )}
+                        {isUnique === false && (
+                            <StyledParagraph $isUnique={isUnique}>
+                                WellPartner ID is not unique. Please choose a different one.
+                            </StyledParagraph>
+                        )}
+                    </>
+                )}
+            </StyledDiv>
         </>
     );
 };
