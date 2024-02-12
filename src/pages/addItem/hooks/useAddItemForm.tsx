24753d8ebaa0badff11532bad3c615bcbb4ddedf
bjornole-kodehode <bjornole.kodehode@gmail.com>
2024-02-12 11:29:42 +0100

    feat: functionality to send more then 1 item

--- src/pages/addItem/hooks/useAddItemForm.tsx
+++ src/pages/addItem/hooks/useAddItemForm.tsx
@@ -1,7 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useContext, useEffect } from 'react';
 import AppContext from '../../../contexts/AppContext';
-import { v4 as uuid } from 'uuid';
 import { useForm } from 'react-hook-form';
 import { useLocation } from 'react-router-dom';
 import { ItemTemplate } from '../../../services/apiTypes.ts';
@@ -20,14 +19,15 @@ const defaultTemplate: TemplateSchema = {
 };
 
 const defaultValues: ItemSchema = {
-    wpId: '',
-    serialNumber: '',
+    wpId: [],
+    serialNumber: [],
     vendorId: '',
     locationId: '',
     itemTemplateId: '',
     comment: '',
     createdById: '',
     uniqueWpId: false,
+    uniqueSerialNumber: false,
     isBatch: false,
     preCheck: { check: false, comment: '' },
     documentation: false,
@@ -94,58 +94,38 @@ export const useAddItemForm = () => {
                     type,
                     description,
                 } = await templateSubmit();
-                if (data.isBatch) {
-                    const numberOfItems = parseInt(data.numberOfItems);
-                    const items = [];
-                    for (let i = 0; i < numberOfItems; i++) {
-                        const uniqueWpId = uuid().slice(0, 8);
-                        items.push({
-                            ...data,
-                            itemTemplateId,
-                            wpId: uniqueWpId,
-                            itemTemplate: {
-                                id: itemTemplateId,
-                                type,
-                                categoryId,
-                                productNumber,
-                                description,
-                                createdById: currentUser!.id,
-                            },
-                        });
-                    }
-                    mutate({
-                        items,
-                        files: undefined,
-                    });
-                }
-                mutate({
-                    items: [
-                        {
-                            ...data,
-                            itemTemplate: {
-                                id: itemTemplateId,
 
-                                type: type,
-                                categoryId: categoryId,
-                                productNumber: productNumber,
-
-                                description: description,
-                                createdById: currentUser?.id ?? '',
-                            },
-                            itemTemplateId,
-                        },
-                    ],
-                    files: undefined,
-                });
-            } else if (data.isBatch) {
                 const numberOfItems = parseInt(data.numberOfItems);
                 const items = [];
                 for (let i = 0; i < numberOfItems; i++) {
-                    const uniqueWpId = uuid().slice(0, 8);
+                    items.push({
+                        ...data,
+                        itemTemplateId,
+                        wpId: data.wpId[i] as unknown as string[],
+                        serialNumber: data.serialNumber[i] as unknown as string[],
+                        itemTemplate: {
+                            id: itemTemplateId,
+                            type,
+                            categoryId,
+                            productNumber,
+                            description,
+                            createdById: currentUser!.id,
+                        },
+                    });
+                }
+                mutate({
+                    items,
+                    files: undefined,
+                });
+            } else {
+                const numberOfItems = parseInt(data.numberOfItems);
+                const items = [];
+                for (let i = 0; i < numberOfItems; i++) {
                     items.push({
                         ...data,
                         itemTemplateId: data.itemTemplate.id,
-                        wpId: uniqueWpId,
+                        wpId: data.wpId[i] as unknown as string[],
+                        serialNumber: data.serialNumber[i] as unknown as string[],
                         createdById: currentUser!.id,
                     });
                 }
