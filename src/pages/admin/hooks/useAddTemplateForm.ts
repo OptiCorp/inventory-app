336a031be5df31525ef5319cb0f7a5d45dba0e7d
bjornole-kodehode <bjornole.kodehode@gmail.com>
2024-02-13 15:06:59 +0100

    feat: hook for add template

---
+++ src/pages/admin/hooks/useAddTemplateForm.ts
@@ -0,0 +1,52 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useAddItemTemplate } from '../../../services/hooks/template/useAddItemTemplate';
+import { TemplateSchema, templateSchema } from './templateValidator';
+import { useContext } from 'react';
+import AppContext from '../../../contexts/AppContext';
+import { useForm } from 'react-hook-form';
+
+const defaultValues: TemplateSchema = {
+    productNumber: '',
+    categoryId: '',
+    createdById: '',
+    description: '',
+    revision: '',
+    type: '',
+};
+
+export const useAddTemplateForm = () => {
+    const { mutate } = useAddItemTemplate();
+    const { currentUser, setSnackbarText } = useContext(AppContext);
+
+    const methods = useForm<TemplateSchema>({
+        resolver: zodResolver(templateSchema),
+        defaultValues: {
+            ...defaultValues,
+            createdById: currentUser?.id,
+        },
+    });
+    const { handleSubmit, control, reset, resetField, register, watch } = methods;
+    const onSubmit = handleSubmit((data) => {
+        /* console.table(data); */
+        mutate(data, {
+            onSuccess: () => {
+                /* TODO: Add snackbar if status code 400/500 etc */
+                /* reset({
+                    ...defaultValues,
+                    createdById: data.createdById,
+                }); */
+                setSnackbarText(`Template: ${data.revision}-${data.productNumber} created`);
+            },
+        });
+    });
+
+    return {
+        methods,
+        onSubmit,
+        control,
+        reset,
+        resetField,
+        register,
+        watch,
+    };
+};
