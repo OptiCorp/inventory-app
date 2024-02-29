6cd57ee7427dcdcbbfbc444b4cebb20e68de9f18
bjornole-kodehode <bjornole.kodehode@gmail.com>
2024-02-29 07:16:38 +0100

    fix: optional chaining in case of missing property

--- src/components/ItemDetails/Entry.tsx
+++ src/components/ItemDetails/Entry.tsx
@@ -3,10 +3,10 @@ import { LogEntry } from '../../services/apiTypes';
 import { Typography } from '@mui/material';
 
 export const Entry = ({ entry }: { entry: LogEntry }) => {
-    const date = format(new Date(entry.createdDate), 'dd.MM.yyyy').toString();
+    const date = format(new Date(entry?.createdDate), 'dd.MM.yyyy').toString();
     return (
         <Typography>
-            {`${date}: ${entry.message} by ${entry.createdBy.firstName} ${entry.createdBy.lastName}`}
+            {`${date}: ${entry?.message} by ${entry?.createdBy?.firstName} ${entry?.createdBy?.lastName}`}
         </Typography>
     );
 };
