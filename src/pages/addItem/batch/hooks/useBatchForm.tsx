cc9403ca4ea1751b9be2e73c028e2aaab9f281d8
Eirik Bakkan <eirikbakkan@gmail.com>
2024-03-04 19:29:33 +0100

    fixed bug where no wpid and serial number were shown if not clicking up and down on choose how many items to add

--- src/pages/addItem/batch/hooks/useBatchForm.tsx
+++ src/pages/addItem/batch/hooks/useBatchForm.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useEffect } from 'react';
 import { useController, useFormContext } from 'react-hook-form';
 import { v4 as uuid } from 'uuid';
 import { ItemSchema } from '../../hooks/itemValidator';
@@ -29,6 +29,11 @@ export const useBatchForm = () => {
         setValue('wpId', uniqueWpIds);
         setValue('serialNumber', uniqueSerialNumbers);
     };
+
+    useEffect(() => {
+        setValue('wpId', [uuid().slice(0, 8)]);
+        setValue('serialNumber', [uuid().slice(0, 8)]);
+    }, []);
 
     return {
         isBatch: value,
