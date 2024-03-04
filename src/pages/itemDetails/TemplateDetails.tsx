ffd218e11d44081c33d7d000849f7041fdedca7c
bjornole-kodehode <bjornole.kodehode@gmail.com>
2024-03-04 13:47:13 +0100

    feat: (wip) template page

---
+++ src/pages/itemDetails/TemplateDetails.tsx
@@ -0,0 +1,21 @@
+import { FormProvider } from 'react-hook-form';
+import { TemplateInfo } from './TemplateInfo';
+import { useUpdateItemForm } from './itemInfo/hooks';
+import { useParams } from 'react-router-dom';
+import { useGetItemById } from '../../services/hooks/items/useGetItemById';
+import { StyledContainerDiv } from './styles';
+
+export const TemplateDetails = () => {
+    const { itemId } = useParams();
+    console.log('itemId', itemId);
+    const { data: item } = useGetItemById(itemId!);
+    const { methods } = useUpdateItemForm(item!);
+
+    return (
+        <StyledContainerDiv>
+            <FormProvider {...methods}>
+                <TemplateInfo />
+            </FormProvider>
+        </StyledContainerDiv>
+    );
+};
